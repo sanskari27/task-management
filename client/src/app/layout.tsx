8edd4d5ce33cd41c440c2dc8aea1b5857bffe688
Sanskar <64313600+sanskari27@users.noreply.github.com>
2024-07-15 13:22:09 +0530

    chore: Add lazy loading for images and update layout

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { BackgroundBeams } from '@/components/ui/background-beams';
 import { ThemeToggle } from '@/components/ui/theme-toggle';
 import { cn } from '@/lib/utils';
 import type { Metadata } from 'next';
@@ -19,13 +20,14 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang='en'>
-			<body className={cn(inter.className)}>
+			<body className={cn(inter.className, 'h-screen w-screen')}>
 				<Providers>
 					<div className='absolute top-3 right-3'>
 						<ThemeToggle />
 					</div>
 					{children}
 				</Providers>
+				<BackgroundBeams />
 			</body>
 		</html>
 	);
