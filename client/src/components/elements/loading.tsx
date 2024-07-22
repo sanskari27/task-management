2474b58fad4e00a1cc7993cd53efdc6a32fe54a5
Sanskar <64313600+sanskari27@users.noreply.github.com>
2024-07-22 17:41:50 +0530

    Refactor code to add react-audio-voice-recorder package for voice note input dialog

--- client/src/components/elements/loading.tsx
+++ client/src/components/elements/loading.tsx
@@ -2,8 +2,10 @@ import { Loader2 } from 'lucide-react';
 
 export default function Loading() {
 	return (
-		<div className='w-screen h-screen absolute left-0 top-0 right-0 bottom-0 flex justify-center items-center  backdrop-blur-sm'>
-			<Loader2 className='h-6 w-6 animate-spin' />
+		<div className='w-screen h-screen absolute left-0 top-0 right-0 bottom-0 flex justify-center items-center  backdrop-blur-sm z-[9999]' onClick={(e)=>e.stopPropagation()}>
+			<div className='p-4 bg-black/40 rounded-full'>
+				<Loader2 className='h-6 w-6 animate-spin' />
+			</div>
 		</div>
 	);
 }
