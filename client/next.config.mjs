0d62b075b553742eb044fbffc62bd4116fd37102
Sanskar <64313600+sanskari27@users.noreply.github.com>
2024-07-24 23:35:33 +0530

    chore: Update hostname in next.config.mjs to use task-api.wautopilot.com

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -13,7 +13,7 @@ const nextConfig = {
 			},
 			{
 				protocol: 'https',
-				hostname: 'api.keethjewels.com',
+				hostname: 'task-api.wautopilot.com',
 				port: '',
 				pathname: '/media/**',
 			},
