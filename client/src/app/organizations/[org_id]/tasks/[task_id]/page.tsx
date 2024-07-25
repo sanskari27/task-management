3e1dc57acbc2b0acefd66984eeb19fa1bef13fb2
Sanskar <64313600+sanskari27@users.noreply.github.com>
2024-07-25 23:07:49 +0530

    Refactor code to add task update functionality

--- client/src/app/organizations/[org_id]/tasks/[task_id]/page.tsx
+++ client/src/app/organizations/[org_id]/tasks/[task_id]/page.tsx
@@ -4,6 +4,7 @@ import AudioPlayer from '@/components/elements/audio-player';
 import ProfileHover from '@/components/elements/profile-hover';
 import { Badge } from '@/components/ui/badge';
 import { LinkPreview } from '@/components/ui/link-preview';
+import { Separator } from '@/components/ui/separator';
 import TasksService from '@/services/tasks.service';
 import {
 	AlarmClock,
@@ -70,160 +71,248 @@ export default async function TaskPage({
 				</Show.When>
 			</Show>
 			<div className='flex flex-col md:flex-row gap-3 mt-6'>
-				<div className='flex-1 flex flex-col gap-3'>
-					<div className='flex flex-col gap-1 border border-dashed p-4 w-full  rounded-xl'>
-						<div className='flex'>
-							<span className='font-medium w-[80px] md:w-[150px]'>Due Date</span>
-							<span className='font-medium mx-3 md:mx-6'>:</span>
-							<p className='inline-flex items-center gap-2 font-medium md:font-bold capitalize'>
-								<AlarmClock size={16} strokeWidth={2.25} />
-								{details.due_date}
-							</p>
-						</div>
-						<div className='flex'>
-							<span className='font-medium w-[80px] md:w-[150px]'>Time Diff</span>
-							<span className='font-medium mx-3 md:mx-6'>:</span>
-							<p className='inline-flex items-center gap-2 font-medium md:font-bold capitalize'>
-								<Hourglass size={16} strokeWidth={2.25} />
-								{details.relative_date}
-							</p>
-						</div>
-						<div className='flex'>
-							<span className='font-medium w-[80px] md:w-[150px]'>Category</span>
-							<span className='font-medium mx-3 md:mx-6'>:</span>
-							<p className='inline-flex items-center gap-2 font-medium md:font-bold capitalize'>
-								<Tag size={16} strokeWidth={2.25} />
-								{details.category}
-							</p>
-						</div>
-						<div className='flex'>
-							<span className='font-medium w-[80px] md:w-[150px]'>Priority</span>
-							<span className='font-medium mx-3 md:mx-6'>:</span>
-							<p className='inline-flex items-center gap-2 font-medium md:font-bold capitalize'>
-								<Flag size={16} strokeWidth={2.25} />
-								{details.priority}
-							</p>
-						</div>
-						<div className='flex'>
-							<span className='font-medium w-[80px] md:w-[150px]'>Status</span>
-							<span className='font-medium mx-3 md:mx-6'>:</span>
-							<p className='inline-flex items-center gap-2 font-medium md:font-bold capitalize'>
-								<Rss size={16} strokeWidth={2.25} />
-								{details.status.replace('_', ' ')}
-							</p>
-						</div>
-					</div>
-					<div className='flex flex-col gap-1 border border-dashed p-4 w-full  rounded-xl'>
-						<div className='flex'>
-							<span className='font-medium w-[110px] md:w-[150px]'>Assigned By</span>
-							<span className='font-medium mx-3 md:mx-6'>:</span>
-							<ProfileHover {...details.created_by}>
+				<div className='flex-1'>
+					<div className='flex-1 flex flex-col gap-3 border-r-0 border-b md:border-b-0 md:border-r border-dotted md:pr-3 md:pb-0 pb-3'>
+						<div className='flex flex-col gap-1 border border-dashed p-4 w-full  rounded-xl'>
+							<div className='flex'>
+								<span className='font-medium w-[80px] md:w-[150px]'>Due Date</span>
+								<span className='font-medium mx-3 md:mx-6'>:</span>
 								<p className='inline-flex items-center gap-2 font-medium md:font-bold capitalize'>
-									<CircleUserRound size={16} strokeWidth={2.25} />
-									{details.created_by.name}
+									<AlarmClock size={16} strokeWidth={2.25} />
+									{details.due_date}
 								</p>
-							</ProfileHover>
+							</div>
+							<div className='flex'>
+								<span className='font-medium w-[80px] md:w-[150px]'>Time Diff</span>
+								<span className='font-medium mx-3 md:mx-6'>:</span>
+								<p className='inline-flex items-center gap-2 font-medium md:font-bold capitalize'>
+									<Hourglass size={16} strokeWidth={2.25} />
+									{details.relative_date}
+								</p>
+							</div>
+							<div className='flex'>
+								<span className='font-medium w-[80px] md:w-[150px]'>Category</span>
+								<span className='font-medium mx-3 md:mx-6'>:</span>
+								<p className='inline-flex items-center gap-2 font-medium md:font-bold capitalize'>
+									<Tag size={16} strokeWidth={2.25} />
+									{details.category}
+								</p>
+							</div>
+							<div className='flex'>
+								<span className='font-medium w-[80px] md:w-[150px]'>Priority</span>
+								<span className='font-medium mx-3 md:mx-6'>:</span>
+								<p className='inline-flex items-center gap-2 font-medium md:font-bold capitalize'>
+									<Flag size={16} strokeWidth={2.25} />
+									{details.priority}
+								</p>
+							</div>
+							<div className='flex'>
+								<span className='font-medium w-[80px] md:w-[150px]'>Status</span>
+								<span className='font-medium mx-3 md:mx-6'>:</span>
+								<p className='inline-flex items-center gap-2 font-medium md:font-bold capitalize'>
+									<Rss size={16} strokeWidth={2.25} />
+									{details.status.replace('_', ' ')}
+								</p>
+							</div>
 						</div>
-						<div className='flex'>
-							<span className='font-medium w-[110px] md:w-[150px]'>Assigned To</span>
-							<span className='font-medium mx-3 md:mx-6'>:</span>
+						<div className='flex flex-col gap-1 border border-dashed p-4 w-full  rounded-xl'>
+							<div className='flex'>
+								<span className='font-medium w-[110px] md:w-[150px]'>Assigned By</span>
+								<span className='font-medium mx-3 md:mx-6'>:</span>
+								<ProfileHover {...details.created_by}>
+									<p className='inline-flex items-center gap-2 font-medium md:font-bold capitalize'>
+										<CircleUserRound size={16} strokeWidth={2.25} />
+										{details.created_by.name}
+									</p>
+								</ProfileHover>
+							</div>
+							<div className='flex'>
+								<span className='font-medium w-[110px] md:w-[150px]'>Assigned To</span>
+								<span className='font-medium mx-3 md:mx-6'>:</span>
+								<ul>
+									<Each
+										items={details.assigned_to}
+										render={(item) => (
+											<li>
+												<ProfileHover {...item}>
+													<p className='inline-flex items-center gap-2 font-medium md:font-bold capitalize'>
+														<CircleUserRound size={16} strokeWidth={2.25} />
+														{item.name}
+													</p>
+												</ProfileHover>
+											</li>
+										)}
+									/>
+								</ul>
+							</div>
+						</div>
+						<div className='border border-dashed p-4 w-full  rounded-xl'>
+							<span className='font-semibold w-[80px] md:w-[150px] underline underline-offset-1'>
+								Task Description
+							</span>
+							<p>{details.description}</p>
+						</div>
+						<div
+							className='border border-dashed p-4 w-full  rounded-xl'
+							hidden={details.links.length === 0}
+						>
+							<span className='font-semibold w-[80px] md:w-[150px] underline underline-offset-1'>
+								Links
+							</span>
 							<ul>
 								<Each
-									items={details.assigned_to}
-									render={(item) => (
+									items={details.links}
+									render={(link) => (
 										<li>
-											<ProfileHover {...item}>
-												<p className='inline-flex items-center gap-2 font-medium md:font-bold capitalize'>
-													<CircleUserRound size={16} strokeWidth={2.25} />
-													{item.name}
-												</p>
-											</ProfileHover>
+											<LinkPreview
+												url={link}
+												className='font-medium md:font-semibold text-sm md:text-base bg-clip-text text-transparent dark:text-transparent bg-gradient-to-br from-purple-500 to-pink-500 dark:from-lime-300 dark:to-emerald-300'
+											>
+												{link}
+											</LinkPreview>
+										</li>
+									)}
+								/>
+							</ul>
+						</div>
+						<div
+							className='border border-dashed p-4 w-full  rounded-xl'
+							hidden={details.files.length === 0}
+						>
+							<span className='font-semibold w-[80px] md:w-[150px] underline underline-offset-1'>
+								Files
+							</span>
+							<ul className='flex flex-col mt-2'>
+								<Each
+									items={details.files}
+									render={(file, index) => (
+										<li>
+											<a
+												href={`${process.env.NEXT_PUBLIC_API_URL}media/${file}`}
+												target='_blank'
+												rel='noreferrer'
+												className='inline-flex gap-2 items-center line-clamp-1'
+											>
+												{getComponentByMimeType(file)}
+												{`File ${index + 1}.${file.split('.').pop()}`}
+											</a>
+										</li>
+									)}
+								/>
+							</ul>
+						</div>
+						<div
+							className='border border-dashed p-4 w-full  rounded-xl'
+							hidden={details.voice_notes.length === 0}
+						>
+							<span className='font-semibold w-[80px] md:w-[150px] underline underline-offset-1'>
+								Voice Notes
+							</span>
+							<ul className='flex flex-col mt-2'>
+								<Each
+									items={details.voice_notes}
+									render={(file, index) => (
+										<li className='inline-flex gap-2 items-center'>
+											<AudioPlayer src={`${process.env.NEXT_PUBLIC_API_URL}media/${file}`} />
+
+											<AudioLines size={16} strokeWidth={2.25} />
+											{`Voice Note ${index + 1}`}
 										</li>
 									)}
 								/>
 							</ul>
 						</div>
 					</div>
-					<div className='border border-dashed p-4 w-full  rounded-xl'>
-						<span className='font-semibold w-[80px] md:w-[150px] underline underline-offset-1'>
-							Task Description
-						</span>
-						<p>{details.description}</p>
-					</div>
-					<div
-						className='border border-dashed p-4 w-full  rounded-xl'
-						hidden={details.links.length === 0}
-					>
-						<span className='font-semibold w-[80px] md:w-[150px] underline underline-offset-1'>
-							Links
-						</span>
-						<ul>
-							<Each
-								items={details.links}
-								render={(link) => (
-									<li>
-										<LinkPreview
-											url={link}
-											className='font-medium md:font-semibold text-sm md:text-base bg-clip-text text-transparent dark:text-transparent bg-gradient-to-br from-purple-500 to-pink-500 dark:from-lime-300 dark:to-emerald-300'
-										>
-											{link}
-										</LinkPreview>
-									</li>
-								)}
-							/>
-						</ul>
-					</div>
-					<div
-						className='border border-dashed p-4 w-full  rounded-xl'
-						hidden={details.files.length === 0}
-					>
-						<span className='font-semibold w-[80px] md:w-[150px] underline underline-offset-1'>
-							Files
-						</span>
-						<ul className='flex flex-col mt-2'>
-							<Each
-								items={details.files}
-								render={(file, index) => (
-									<li>
-										<a
-											href={`${process.env.NEXT_PUBLIC_API_URL}media/${file}`}
-											target='_blank'
-											rel='noreferrer'
-											className='inline-flex gap-2 items-center line-clamp-1'
-										>
-											{getComponentByMimeType(file)}
-											{`File ${index + 1}.${file.split('.').pop()}`}
-										</a>
-									</li>
-								)}
-							/>
-						</ul>
-					</div>
-					<div
-						className='border border-dashed p-4 w-full  rounded-xl'
-						hidden={details.voice_notes.length === 0}
-					>
-						<span className='font-semibold w-[80px] md:w-[150px] underline underline-offset-1'>
-							Voice Notes
-						</span>
-						<ul className='flex flex-col mt-2'>
-							<Each
-								items={details.voice_notes}
-								render={(file, index) => (
-									<li className='inline-flex gap-2 items-center'>
-										<AudioPlayer src={`${process.env.NEXT_PUBLIC_API_URL}media/${file}`} />
-
-										<AudioLines size={16} strokeWidth={2.25} />
-										{`Voice Note ${index + 1}`}
-									</li>
-								)}
-							/>
-						</ul>
-					</div>
 				</div>
-				<div className='flex-1 flex flex-col gap-3 border-l-0 border-t md:border-t-0 md:border-l border-dotted md:pl-3 md:pt-0 pt-3'>
-					<h3 className='text-xl font-semibold'>Task Updates</h3>
-					{updates.length} updates found
+				<div className='flex-1 flex flex-col gap-2  max-h-fit overflow-scroll'>
+					<div className='flex justify-between items-end'>
+						<h3 className='text-xl font-semibold'>Task Updates</h3>
+						<span className='text-sm'>{updates.length} updates found</span>
+					</div>
+					<div className='flex flex-col gap-2 overflow-y-scroll'>
+						<Each
+							items={updates}
+							render={(update) => (
+								<div className='flex flex-col gap-2 border border-dashed p-4 w-full  rounded-xl'>
+									<div className='flex'>
+										<ProfileHover {...update.added_by}>
+											<p className='inline-flex items-center gap-2 font-medium md:font-bold capitalize'>
+												<CircleUserRound size={16} strokeWidth={2.25} />
+												{update.added_by.name}
+											</p>
+										</ProfileHover>
+									</div>
+									<div>
+										<p>{update.message}</p>
+									</div>
+									<div hidden={update.links.length === 0}>
+										<Separator className='mb-2' />
+										<span className='font-semibold w-[80px] md:w-[150px] underline underline-offset-1'>
+											Links
+										</span>
+										<ul>
+											<Each
+												items={update.links}
+												render={(link) => (
+													<li>
+														<LinkPreview
+															url={link}
+															className='font-medium md:font-semibold text-sm md:text-base bg-clip-text text-transparent dark:text-transparent bg-gradient-to-br from-purple-500 to-pink-500 dark:from-lime-300 dark:to-emerald-300'
+														>
+															{link}
+														</LinkPreview>
+													</li>
+												)}
+											/>
+										</ul>
+									</div>
+									<div hidden={update.files.length === 0}>
+										<Separator className='mb-2' />
+										<span className='font-semibold w-[80px] md:w-[150px] underline underline-offset-1 '>
+											Files
+										</span>
+										<ul className='flex flex-col mt-2'>
+											<Each
+												items={update.files}
+												render={(file, index) => (
+													<li>
+														<a
+															href={`${process.env.NEXT_PUBLIC_API_URL}media/${file}`}
+															target='_blank'
+															rel='noreferrer'
+															className='inline-flex gap-2 items-center line-clamp-1'
+														>
+															{getComponentByMimeType(file)}
+															{`File ${index + 1}.${file.split('.').pop()}`}
+														</a>
+													</li>
+												)}
+											/>
+										</ul>
+									</div>
+									<div hidden={update.voice_notes.length === 0}>
+										<Separator className='mb-2' />
+										<span className='font-semibold w-[80px] md:w-[150px] underline underline-offset-1 '>
+											Voice Notes
+										</span>
+										<ul className='flex flex-col mt-2'>
+											<Each
+												items={update.voice_notes}
+												render={(file, index) => (
+													<li className='inline-flex gap-2 items-center'>
+														<AudioPlayer src={`${process.env.NEXT_PUBLIC_API_URL}media/${file}`} />
+
+														<AudioLines size={16} strokeWidth={2.25} />
+														{`Voice Note ${index + 1}`}
+													</li>
+												)}
+											/>
+										</ul>
+									</div>
+								</div>
+							)}
+						/>
+					</div>
 				</div>
 			</div>
 		</section>
