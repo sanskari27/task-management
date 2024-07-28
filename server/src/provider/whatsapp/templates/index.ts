cdee429a73e4e19f1dc44cfb2a2262e761006031
Sanskar <64313600+sanskari27@users.noreply.github.com>
2024-07-28 16:43:43 +0530

    chore: Add taskCreate template for WhatsApp messaging

--- server/src/provider/whatsapp/templates/index.ts
+++ server/src/provider/whatsapp/templates/index.ts
@@ -1,11 +1,13 @@
 import { default as taskOverview } from './taskOverview';
 import { default as taskReminder } from './taskReminder';
 import { default as taskUpdate } from './taskUpdate';
+import { default as taskCreate } from './taskCreate';
 
 const WhatsappTemplates = {
 	taskReminder,
 	taskOverview,
 	taskUpdate,
+	taskCreate,
 };
 
 export default WhatsappTemplates;
